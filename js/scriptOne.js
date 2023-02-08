26a2623b849287dbf8336c1e1ba8921008fae225
karlahernande <hernandezkh.5@yahoo.com>
2023-02-07 20:34:04 -0800

    Karla H
    Attempt to do the calculator only working on the calculator to finish

---
+++ js/scriptOne.js
@@ -0,0 +1,9 @@
+{/* <script>
+function multiplyNumbers(x,y) {
+    return x*y;
+}
+var z = multiplyNumbers(4,5); 
+
+</script>
+<button onclick="document.getElementById('multiplication').inerHTML=z;">Multiply numbers</button>
+<p id="multiplication"></p> */}
